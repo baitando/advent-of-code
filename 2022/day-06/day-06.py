036599ef3353e3b9eeefec3776b25af217ab34c3
Andreas Rehmann <andreas.rehmann@mimacom.com>
2022-12-06 06:19:33 +0100

    Day 6 in 2022

--- 2022/day-06/day-06.py
+++ 2022/day-06/day-06.py
@@ -11,22 +11,18 @@ def all_distinct(lis):
     return len(set(lis)) == len(lis)
 
 
-def part1():
-    data = read_lines()[0]
+def get_unique_seq_index(data, length):
+    for index in range(0, len(data) - length):
+        if all_distinct(data[index:index + length]):
+            return index + 4
 
-    for index in range(0, len(data) - 4):
-        if all_distinct(data[index:index + 4]):
-            print("Part 1: " + str(index + 4))
-            break
+
+def part1():
+    print("Part 1: " + str(get_unique_seq_index(read_lines()[0], 4)))
 
 
 def part2():
-    data = read_lines()[0]
-
-    for index in range(0, len(data) - 14):
-        if all_distinct(data[index:index + 14]):
-            print("Part 1: " + str(index + 14))
-            break
+    print("Part 2: " + str(get_unique_seq_index(read_lines()[0], 14)))
 
 
 part1()
